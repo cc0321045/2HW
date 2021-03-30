1cc25c89054958556716ba39b276ddc239c80a6d
cc0321045 <adam0012367@gmail.com>
2021-03-30 15:11:28 +0800

    Update Default.aspx.cs

--- 2HW/Default.aspx.cs
+++ 2HW/Default.aspx.cs
@@ -8,7 +8,17 @@ using System.Web.UI.WebControls;
 namespace _2HW {
     public partial class Default : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
+            int g = 12;
+            int sum = 0;
+            for (int a = 0; a <= g; a++)
+            {
+                if (a % 2 == 0 || a % 3 == 0)
+                {
+                    sum = sum + a;
 
+                }
+            }
+            Response.Write(sum);
         }
     }
 }
